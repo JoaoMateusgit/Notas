db7ede15f8ffb4eb16bc3ddabf7f3ac469fa3d89
JoaoMateusgit <joaomateusperachi33@gmail.com>
2023-10-26 18:02:03 -0300

    Adicionar arquivos de projeto.

---
+++ Notas/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using Notas;
+using System.Globalization;
+namespace course
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+
+            boletim a = new boletim();
+
+            Console.WriteLine("digite a primeira nota: ");
+            a.NotaUM = double.Parse(Console.ReadLine());
+            Console.WriteLine("digite a segunda nota: ");
+            a.NotaDois = double.Parse(Console.ReadLine());
+            Console.WriteLine("digite a terceira nota: ");
+            a.NotaTres = double.Parse(Console.ReadLine());
+
+
+
+            if (a.Somar() <= 60)
+            {
+                Console.WriteLine("Reprovado, faltou: " + a.Falta() + " Pontos");
+            }
+            else
+            {
+                Console.WriteLine("Aprovado.");
+            }
+        }
+    }
+}
---
+++ Notas/boletim.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notas
+{
+    internal class boletim
+    {
+        public double NotaUM;
+        public double NotaDois;
+        public double NotaTres;
+
+        public double Somar()
+        {
+            return NotaUM + NotaDois + NotaTres;
+    }
+        public double Falta()
+        {
+            return 60 - Somar() ;
+        }
+
+    }
+}
